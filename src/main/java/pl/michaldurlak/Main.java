79423f30dc5b4ad0a5c64231eda4e4a382242a01
Michał Durlak <durlak.m@wp.pl>
2022-09-13 21:40:14 +0200

    Git init

--- src/main/java/pl/michaldurlak/Main.java
+++ src/main/java/pl/michaldurlak/Main.java
@@ -1,7 +1,8 @@
-package org.example;
+package pl.michaldurlak;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("TEST");
     }
 }
